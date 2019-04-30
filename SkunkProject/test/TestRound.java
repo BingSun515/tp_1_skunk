c32fd515bf7bc34f208bfdf5dca96167dd81b8b8
Hsiang Teng <Hsiang Teng@192.168.86.43>
2019-04-29 23:00:43 -0500

    improving

--- SkunkProject/test/TestRound.java
+++ SkunkProject/test/TestRound.java
@@ -8,8 +8,10 @@ public class TestRound {
 
 	@Test
 	public void getRound() {
-		Round r1 = new Round(new Turn());
-		r1.getScore();
+		Turn t1 = new Turn();
+		Round r1 = new Round(t1);
+		Score s1 =new Score();
+		assertEquals(s1 ,r1.getScore());
 	}
 
 }
