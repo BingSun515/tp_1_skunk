502c83a7a9a66fd14f4bb7e8bc2be0bee23292e9
Hsiang Teng <Hsiang Teng@192.168.86.43>
2019-04-29 22:55:48 -0500

    Keep improving

---
+++ SkunkProject/test/TestRound.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TestRound {
+
+	@Test
+	public void getRound() {
+		Round r1 = new Round(new Turn());
+		r1.getScore();
+	}
+
+}
