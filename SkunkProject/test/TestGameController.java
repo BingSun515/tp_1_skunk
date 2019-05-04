a464ea7c0e1b2fd93d686e4f2faf15fee1fb7020
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-05-04 14:22:27 -0500

    keep improving

--- SkunkProject/test/TestGameController.java
+++ SkunkProject/test/TestGameController.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import org.junit.Test;
 
 public class TestGameController {
@@ -11,17 +15,11 @@ public class TestGameController {
 		assertEquals("Eric", gc1.getPlayerName());
 	}
 	
-//	@Test
-//	public void TestGetPlayerRollChoice() {
-//		GameController gc1 = new GameController();
-//		assertEquals(null, gc1);
-//	}
-	
-//	@Test
-//	public void testStartGame() {
-//		GameController gc1 = new GameController();
-//		gc1.startGame("Eric");
-//		assertEquals("Eric", gc1.getPlayer());
-//	}
+	@Test
+	public void testStartGame() {
+		GameController gc2 = new GameController();
+		gc2.startGame();
+
+	}
 		
 }
